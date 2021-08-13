f1ee54fa4873f1b6e78f9763dac4b9190de6aa8c
Мишаков В.В <programs@KD.local>
2021-08-14 02:29:06 +0300

    fix script

---
+++ install-service.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\ServicesControl" && nssm.exe install ServicesControl "C:\Program Files\ServicesControl\wsc.exe" AppDirectory "C:\Program Files\ServicesControl" && net start ServicesControl
+pause
